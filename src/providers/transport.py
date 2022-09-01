55d6a51c296d4cb91ea074e9fdd3f7704ef84fb7
nxfive <95922573+nxfive@users.noreply.github.com>
2022-09-01 22:41:56 +0200

    create class TransportCar to transport components from Companies to Warehouse

---
+++ src/providers/transport.py
@@ -0,0 +1,10 @@
+class TransportCar:
+
+    def __init__(self, pallets, order):
+        self.max_capacity = 40
+        self.delivery_note = {key: value for (key, value) in order.items.items()}
+        if self.max_capacity < pallets:
+            self.delivery_cost = (self.max_capacity - pallets) * 100
+            print(f"[ADDITIONAL FEE] {self.delivery_cost}")
+        self.indexes = [key for key in order.items.keys()]
+        self.number_of_pallets = pallets
