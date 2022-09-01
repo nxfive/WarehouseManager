c2ecff595037900082dbd6201033ec076b8596b9
nxfive <95922573+nxfive@users.noreply.github.com>
2022-09-01 22:47:55 +0200

    structure of main module

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import asyncio
+from src.warehouse.warehouse import Warehouse
+
+tasks = []
+
+
+async def transport():
+    pass
+
+
+async def do_the_tasks():
+    pass
+
+
+async def production():
+    pass
