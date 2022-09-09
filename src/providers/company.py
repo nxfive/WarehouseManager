f776d0e81669bfb0546bad9d7964b39529a3552c
nxfive <95922573+nxfive@users.noreply.github.com>
2022-09-09 18:42:10 +0200

    moved saving to file func to the class FileMaker in utils.py

--- src/providers/company.py
+++ src/providers/company.py
@@ -2,9 +2,7 @@ from src.providers.component import Component
 from src.providers.transport import TransportCar
 import random
 import math
-import os
-import errno
-import json
+from src.utils.utils import FileMaker as Fm
 
 
 class Company:
@@ -15,30 +13,10 @@ class Company:
         with Component() as component:
             for _ in range(amount):
                 self.components.append(component)
+        Fm.save_items_in_json(self)
         self.pallet_per_component = {key: random.choice([0.25, 0.5, 0.75, 1]) for key in self.components}
         self.charge_for_pallet_per_component = {key: (1 - value) / value * random.randint(500, 5000)
                                                 for (key, value) in self.pallet_per_component.items()}
-
-    def add_components_to_file(self):
-        """
-            This function creates a directory for storing components and later created invoice files.
-        """
-        dir_path = f'{self.name}'
-        try:
-            os.makedirs(dir_path, 644)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        filename = f'{self.name}_components.json'
-        with open(os.path.join(dir_path, filename), 'w') as file:
-            for component in self.components:
-                file.write(json.dumps(component, default=Company.encoder_component, indent=4))
-
-    @staticmethod
-    def encoder_component(component):
-        if isinstance(component, Component):
-            return {'index': component.index_number, 'quantity': component.quantity}
-        raise TypeError(f'Object {component} is not of type Component.')
 
     def check_order(self, order):
         pallets = 0
@@ -53,16 +31,9 @@ class Company:
         return transport
 
     def issue_an_invoice(self, order):
-        """
-            This function creates a directory for storing created invoice files.
-        """
-        dir_path = f'{self.name}'
-        with open(os.path.join(dir_path, f'{order.identity}.txt'), 'w') as file:
-            invoice = Invoice(order, self)
-            file.write(f'Invoice number: {invoice.number}\n')
-            for index, item, payment in enumerate(invoice.items_payment.items()):
-                file.write(f'{index+1}. Item id: {item.identity} - {payment} PLN.\n')
-            file.write(f'Sum: {invoice.payment} PLN\n')
+        invoice = Invoice(order, self)
+        Fm.save_invoice_to_text_file(self, invoice)
+        return
 
     @property
     def name(self):
