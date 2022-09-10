ccce6f8cfa21f393ab9461e892111a0b54724189
nxfive <95922573+nxfive@users.noreply.github.com>
2022-09-10 08:44:16 +0200

    add warehouse to __init__ to make attr, and add attr change and job so class WarehouseOperator and Leader can inherit

--- src/person/worker.py
+++ src/person/worker.py
@@ -1,22 +1,25 @@
 from src.person.person import Person
-from src.utils import validation_input as validate
+from src.utils.utils import validation_input as validate
 from src.warehouse.task import Task
+import random
 
 
 class Worker(Person):
 
     """Worker class is for workers w/o driver licence"""
 
-    def __init__(self, name, surname, age, experience):
+    def __init__(self, warehouse, name, surname, age, experience):
+        super().__init__(name, surname, age, experience)
         validate(name, 'name')
         validate(surname, 'surname')
         validate(age, 'age')
         self._name = name
         self._surname = surname
         self._age = age
+        self._warehouse = warehouse
         self._experience = experience
         self._identity = id(self)
-        self.tasks = Task.tasks
+        self._change, self._job = self.get_change_and_job(warehouse)
 
     def __str__(self):
         return f'[{self.__class__.__name__}] Name: {self.name}, Surname: {self.surname}'
@@ -35,3 +38,23 @@ class Worker(Person):
     @property
     def identity(self):
         return self._identity
+
+    @property
+    def warehouse(self):
+        return self._warehouse
+
+    def get_change_and_job(self):
+        structure = self.warehouse.warehouse_structure
+        change = random.choice([key for key in structure.keys()])
+        free_jobs = []
+        for key, val in structure[change].items():
+            for item in val:
+                if item is None:
+                    free_jobs.append(key)
+                    break
+        job = random.choice(free_jobs)
+        for item in structure[change][job]:
+            if item is None:
+                structure[change][job][item] = self
+                break
+        return change, job
